ac468b51423b756ed31d036ce42c8bf26199ac57
buec <m.buechi@outlook.com>
2019-12-06 09:55:58 +0100

    Hash issuers.json separately

--- scripts/equivalence.py
+++ scripts/equivalence.py
@@ -38,7 +38,7 @@ def check(data_folder):
 
     actual_state = generate_state(full_path)
 
-    if actual_state == state:
+    if compare(state, actual_state):
         print('OK - Your data matches the state of the repository')
     else:
         print('NOT OK - Your data differs from the state of the repository')
@@ -55,12 +55,10 @@ def generate_state(path):
             print(' - {} ...'.format(filename))
             file_path = os.path.join(path, filename)
 
-            with open(file_path, 'rb') as f:
-                content = f.read()
-
-            h = hashlib.new('md5')
-            h.update(content)
-            hash_value = h.hexdigest()
+            if filename == 'issuers.json':
+                hash_value = hash_issuers_json(file_path)
+            else:
+                hash_value = hash_file(file_path)
 
             state['meta_files'][filename] = hash_value
 
@@ -81,6 +79,46 @@ def generate_state(path):
     state['audio_files'] = hash_value
 
     return state
+
+
+def hash_file(path):
+    with open(path, 'rb') as f:
+        content = f.read()
+
+    h = hashlib.new('md5')
+    h.update(content)
+    return h.hexdigest()
+
+
+def hash_issuers_json(path):
+    with open(path, 'r') as f:
+        content = json.load(f)
+
+    h = hashlib.new('md5')
+
+    for x, y in sorted(content.items(), key=lambda x: x[0]):
+        parts = [x]
+        for k, v in sorted(y.items(), key=lambda x: x[0]):
+            parts.append(k)
+            parts.append(v)
+
+    h.update(' '.join(parts).encode('utf-8'))
+    return h.hexdigest()
+
+
+def compare(reference, actual):
+    ok = True
+
+    for meta_file, hash_value in reference['meta_files'].items():
+        if hash_value != actual['meta_files'][meta_file]:
+            print('Hash value of {} differs'.format(meta_file))
+            ok = False
+
+    if reference['audio_files'] != actual['audio_files']:
+        print('Hash value of audio files differs')
+        ok = False
+
+    return ok
 
 
 if __name__ == '__main__':
